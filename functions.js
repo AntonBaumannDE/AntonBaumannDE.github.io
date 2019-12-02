4b493bd0ef7ca51be6ffa94d586f7226ee1c0fc8
Anton Baumann <49437200+AntonBaumannDE@users.noreply.github.com>
2019-12-02 16:31:21 +0100

    newnew

---
+++ functions.js
@@ -0,0 +1,17 @@
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  if (wScroll <= pContainerHeight) {
+
+    $('.bannertext').css({
+      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+    });
+
+   
+
+    $('.bannersäule').css({
+      'transform' : 'translate(0px, -'+ wScroll /20 +'%)'
+    });
+
+  });
